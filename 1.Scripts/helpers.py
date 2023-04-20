1601a65f47992a88cb03094a69b7f410203d6c80
Alvee <42814494+ALvee-611@users.noreply.github.com>
2023-04-19 21:52:12 -0400

    CF model completed

--- 1.Scripts/helpers.py
+++ 1.Scripts/helpers.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine, text as sql_text
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+import pickle
+
 config = configparser.ConfigParser()
 config.read('..\\..\\config.ini')
 
@@ -71,16 +73,53 @@ def dispaly_images(article_list,folder_location):
         plt.show()
 
 
-
-def detail_process(user_input):
+def detail_process(user_input, model_nlp, pca_model_path,pca_model_name):
     """
     Preprocess user input and reduce feature size
     """
+    # Locating the pca model location
+    pca_loc = os.path.join(pca_model_path, pca_model_name)
+    
     #Get embeddings for the sentences
     sentence_embeddings = model_nlp.encode(user_input).reshape(1, -1)
+    
     # reduce number of features:
-    # Load the PCA model from file
-    with open('..\\..\\assets\\good_small_pca_model.pkl', 'rb') as f:
+    with open(pca_loc, 'rb') as f:
         pca = pickle.load(f)
     processed = pca.transform(sentence_embeddings)
+    
     return processed
+
+
+## Finding the similar items based on item features:
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+def similar_items(full_df, article_id, num_items = 6):
+    """
+    takes the dataframe full_df containing the articl_id and all the features and the articl_id of the
+    items for which we need similar items, and then return list indices of the num_items(default 5) most similar items
+    similar_items: DataFrame Int Int -> Listof(Int)
+    """
+    # get item index
+    item_index = full_df[full_df['article_id'] == article_id].index[0]
+    # saving the ids
+    id_df = full_df['article_id']
+    # dropping the article_id feature
+    main_df = full_df.drop(columns=['article_id'], axis=1)
+
+    item_A = np.array(main_df.iloc[item_index]).reshape(1, -1)
+
+    # NearestNeighbors model(taking 6 since first item will be the item itself)
+    nbrs = NearestNeighbors(n_neighbors=num_items, algorithm='auto').fit(main_df)
+
+    # get distances and indices of nearest neighbors
+    _, indices = nbrs.kneighbors(item_A)
+
+    # the article_id of 5 most similar items
+    id = list(id_df[indices[0]])
+
+    return id
+
+#example_id = 145872053
+#similar_items(rest_df, example_id)
